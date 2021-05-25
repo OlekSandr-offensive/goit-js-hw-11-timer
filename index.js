e30241320a2eee910ddfaa5ed10467ad5c400361
Oleksandr Tarasiuk <oleksandr.tarasyuk92@gmail.com>
2021-05-25 15:39:35 +0200

    відредагував помилки в коді

--- index.js
+++ index.js
@@ -1,49 +1,64 @@
-const refs = {
-  daysEl: document.querySelector('#timer-1 .value[data-value="days"]'),
-  hoursEl: document.querySelector('#timer-1 .value[data-value="hours"]'),
-  minsEl: document.querySelector('#timer-1 .value[data-value="mins"]'),
-  secsEl: document.querySelector('#timer-1 .value[data-value="secs"]'),
-};
-
 const PROM_DALEY = 1000;
 
 class CountdownTimer {
   constructor({ selector, targetDate }) {
     this.intervalId = null;
-    this.selector = selector;
+    this.selector = this.getSelectorEl(selector);
     this.targetDate = targetDate;
   }
 
   start() {
-    const startTime = this.targetDate;
+    this.countdownTimer();
 
     this.intervalId = setInterval(() => {
-      const currentTime = Date.now();
-      const time = startTime - currentTime;
-
-      const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-      const hours = this.pad(
-        Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-      );
-      const mins = this.pad(
-        Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)),
-      );
-      const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
-
-      this.selector.daysEl.innerHTML = days;
-      this.selector.hoursEl.innerHTML = hours;
-      this.selector.minsEl.innerHTML = mins;
-      this.selector.secsEl.innerHTML = secs;
+      this.countdownTimer();
     }, PROM_DALEY);
+  }
+
+  countdownTimer() {
+    const startTime = this.targetDate;
+    const currentTime = Date.now();
+    const time = startTime - currentTime;
+    const timeComponents = this.getTimeComponent(time);
+
+    this.updateClockFace(timeComponents);
+  }
+
+  getSelectorEl(timeId) {
+    const refs = {
+      daysEl: document.querySelector(`${timeId} [data-value="days"]`),
+      hoursEl: document.querySelector(`${timeId} [data-value="hours"]`),
+      minsEl: document.querySelector(`${timeId} [data-value="mins"]`),
+      secsEl: document.querySelector(`${timeId} [data-value="secs"]`),
+    };
+    return refs;
+  }
+
+  updateClockFace({ days, hours, mins, secs }) {
+    this.selector.daysEl.innerHTML = days;
+    this.selector.hoursEl.innerHTML = hours;
+    this.selector.minsEl.innerHTML = mins;
+    this.selector.secsEl.innerHTML = secs;
   }
 
   pad(value) {
     return String(value).padStart(2, "0");
   }
+
+  getTimeComponent(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
 }
 
 const timer = new CountdownTimer({
-  selector: refs,
+  selector: "#timer-1",
   targetDate: new Date("Jul 17, 2021"),
 });
 
